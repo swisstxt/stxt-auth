5a11389969a1825583eeedf843dca22bf1eb75a0
Sokol <Tatjana.Sokol@swisstxt.ch>
2024-04-18 20:59:23 +0200

    the first commit

---
+++ app/app1/client/src/components/LoginForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function LoginForm() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>{message || "React app von app1"}</p>
+            </header>
+        </div>
+    );
+}
+
+export default LoginForm;
---
+++ app/login/client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+
+  return (
+      <div className="App">
+        <a href="https://app1.example1.com/secure">App 1</a>
+        <a href="https://app2.example2.com/secure">App 2</a>
+        <a href="https://app3.example3.com/secure">App 3</a>
+      </div>
+  );
+}
+
+export default App;
