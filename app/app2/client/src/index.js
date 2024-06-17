bf191789503c315f7d9dbc29faf8e6e59a3d423d
Tatjana Sokol <tatjana.sokol@swisstxt.ch>
2024-06-17 17:38:44 +0200

    add the shared templates

--- app/app2/client/src/index.js
+++ app/app2/client/src/index.js
@@ -13,7 +13,7 @@ root.render(
     >
         <Router>
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<App app="App2"/>} />
             </Routes>
         </Router>
     </ReactKeycloakProvider>
